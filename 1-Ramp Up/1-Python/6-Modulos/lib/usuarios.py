9790317752d672c8a6e2c2471d015f0aea8c3b5e
Javlopalf <lopezalfarojavier@gmail.com>
2022-10-20 10:42:34 +0200

    actualización 20 octubre

---
+++ 1-Ramp Up/1-Python/6-Modulos/lib/usuarios.py
@@ -0,0 +1,19 @@
+
+usuario1 = str(input("Introduce un nombre: "))
+
+
+while True:
+    if usuario1 != str:
+        print("vuelve a introducir tu usuario")
+    else:
+        print(usuario1)
+           
+
+     
+        
+
+
+
+
+
+
